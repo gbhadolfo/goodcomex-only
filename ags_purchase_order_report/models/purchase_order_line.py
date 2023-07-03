9dd1954fc2febadc9d5c88616d8a92fea28b7928
gbhadolfo <adolfo.arbach@gmail.com>
2023-07-03 07:44:56 -0300

    Delete ags_purchase_order_report directory

--- ags_purchase_order_report/models/purchase_order_line.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-
-class PurchaseOrderLine(models.Model):
-    _inherit = 'purchase.order.line'
-    partner_delivery_id = fields.Many2one(comodel_name ="res.partner", string='Entrega')
