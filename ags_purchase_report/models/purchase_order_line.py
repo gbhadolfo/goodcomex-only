41a9e9fd4d20f52717b35794d5264c349fb68ffe
gbhadolfo <adolfo.arbach@gmail.com>
2023-07-06 08:21:34 -0300

    Update purchase_order_line.py

--- ags_purchase_report/models/purchase_order_line.py
+++ ags_purchase_report/models/purchase_order_line.py
@@ -1,6 +1,9 @@
 from odoo import models, fields
 
-
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
     partner_delivery_id = fields.Many2one(comodel_name ="res.partner", string='Entrega')
+    partner_delivery_fantasy_name = fields.Char(related='partner_delivery_id.fantasy_name',string=u'Nombre de fantasía',store=True)
+    
+    def getProductInfo(self, ProductProductID):
+        return self.env['product.product'].browse(ProductProductID)
