f2183cef2d1233ce09e5b3c3fb746f41e9b583b9
gbhadolfo <adolfo.arbach@gmail.com>
2023-07-06 08:22:09 -0300

    Update __manifest__.py

--- ags_purchase_report/__manifest__.py
+++ ags_purchase_report/__manifest__.py
@@ -5,7 +5,7 @@
     'description' : """
     Este modulo incorpora en lineas en la orden de compra el campo cliente, para luego imprimir en el reporte de orden de compras con clientes por producto
     """,
-    'depends' : ['purchase'],
+    'depends' : ['purchase','oct_partner_fantasy_name'],
     'data' : ['views/purchase_order_view.xml'],
     'installable' : True,
     'application' : False,
